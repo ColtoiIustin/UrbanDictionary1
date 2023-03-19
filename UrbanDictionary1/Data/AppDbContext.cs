9f05bc3f241303482b3488c3219eb1f397a85f5a
colto <colto@DESKTOP-2M67LJ5>
2023-03-20 00:29:48 +0200

    dj

--- UrbanDictionary1/Data/AppDbContext.cs
+++ UrbanDictionary1/Data/AppDbContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography.X509Certificates;
 using UrbanDictionary1.Models;
 
 namespace UrbanDictionary1.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options) 
+            : base (options)
         {
             
         }
