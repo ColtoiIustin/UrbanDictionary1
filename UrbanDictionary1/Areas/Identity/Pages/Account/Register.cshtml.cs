5da38dd298b4c4043bc1f631c567c1f57321a049
colto <colto@DESKTOP-2M67LJ5>
2023-04-07 03:46:36 +0300

    poiuy

--- UrbanDictionary1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UrbanDictionary1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -163,8 +163,8 @@ namespace UrbanDictionary1.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                             protocol: Request.Scheme);
 
-                        await SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        await SendEmailAsync(Input.Email, "Activeaza-ti contul",
+                            $"Te rugam sa confirmi adresa de email <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>apasand aici</a>.");
 
                         if (_userManager.Options.SignIn.RequireConfirmedAccount)
                         {
